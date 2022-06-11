090453e415e35590e984968517d1d3fbbdd8e957
leslie liu <liuleslie@wustl.edu>
2022-06-11 11:54:14 -0400

    BIG UPDATE

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+     // this will disable dragging of all images
+     $("img").mousedown(function(e){
+          e.preventDefault()
+     }); 
+
+     // this will disable right-click on all images
+     $("img").on("contextmenu",function(e){
+          return false;
+     });
+});
