39bd8908982fb40033334c77065e19cf6cfd0ade
leslie liu <54601563+liuleslie@users.noreply.github.com>
2023-08-05 01:29:18 -0400

    STUFF IS DRAGGABLE NOW!!!!
    and the square goes wild

---
+++ script2.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+    // $('main img').css({
+    //     'margin-right':(Math.random(20)+5)+'px',
+    // })
+    
+    // $('img').mouseenter(function(){
+    //     $('img').draggable({cursor:"grab"});
+    // });
+
+    $('.draggable').mouseenter(function(){
+        $('.draggable').draggable({cursor:"grab"});
+    });
+
+     
+});
